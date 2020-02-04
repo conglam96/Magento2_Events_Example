237504a87b7571c537d51a41cf3c566781a1ecfe
Lam Nguyen <lamnguyenptit96143@gmai.com>
2020-02-04 09:24:11 +0700

    Magento 2 events example new tab link menu

---
+++ PHPCuong/Menu/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ 
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_Menu',
+    __DIR__
+);
